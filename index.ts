f09df66fb12a57b12ebb59ecfa7d561c2eddb9b9
lukasara3 <lucaoamsilva@gmail.com>
2023-10-13 14:49:43 -0300

    feat(index) : importa funçao CreateMusic no index

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { CreateUser, DeleteUserById, ListAllUsers, UpdateUserData } from "./src/domains/Users/controllers/userController";
 import { Music, User } from "@prisma/client";
+import { CreateMusic } from "./src/domains/Musics/controllers/musicController";
 
 
 
