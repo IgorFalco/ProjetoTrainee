af35892581696b6d0f16a091a3f271c69d3c95aa
Yasmim Lopes <Yasmimel30@gmail.com>
2023-10-14 15:24:39 -0300

    atualizando

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { CreateUser, DeleteUserById, ListAllUsers, UpdateUserData } from "./src/domains/Users/controllers/userController";
 import { Music, User } from "@prisma/client";
 import { CreateMusic, ListAllMusics, UpdateListenerUser, UpdateMusic, UpdateMusicArtist, DeleteMusic} from "./src/domains/Musics/controllers/musicController";
-import { CreateArtist, deleteArtist, updateArtistdata, ArtistStreams } from "./src/domains/controllers/artistController";
+import { CreateArtist, deleteArtist, updateArtist, ArtistStreams } from "./src/domains/controllers/artistController";
 
 async function main() {
 
