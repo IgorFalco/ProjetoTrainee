fb51be7fec9ea19ff1762f819fb0bc66c148551a
lukasara3 <lucaoamsilva@gmail.com>
2023-10-20 00:29:59 -0300

    fix(user): tranformando strings em double quote

--- src/domains/Users/controllers/index.ts
+++ src/domains/Users/controllers/index.ts
@@ -3,58 +3,58 @@ import { Router, Request, Response, NextFunction } from "express";
 
 const router = Router();
 
-router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const users = await userServices.listAll();
-        res.json(users);
-    } catch (error) {
-        next(error);
-    }
-})
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const users = await userServices.listAll();
+		res.json(users);
+	} catch (error) {
+		next(error);
+	}
+});
 
-router.get('/:email', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const user = await userServices.listByEmail(req.params.email);
-        res.json(user);
-    } catch (error) {
-        next(error);
-    }
-})
+router.get("/:email", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const user = await userServices.listByEmail(req.params.email);
+		res.json(user);
+	} catch (error) {
+		next(error);
+	}
+});
 
-router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        await userServices.create(req.body);
-        res.json('Usuário criado com sucesso!');
-    } catch (error) {
-        next('Erro ao criar um novo Usuário' + error);
-    }
-})
+router.post("/create", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await userServices.create(req.body);
+		res.json("Usuário criado com sucesso!");
+	} catch (error) {
+		next("Erro ao criar um novo Usuário" + error);
+	}
+});
 
-router.put('/update/:id', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        await userServices.update(parseInt(req.params.id), req.body);
-        res.json('Usuário atualizado com sucesso!');
-    } catch (error) {
-        next('Erro ao atualizar o  Usuário' + error);
-    }
-})
+router.put("/update/:id", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await userServices.update(parseInt(req.params.id), req.body);
+		res.json("Usuário atualizado com sucesso!");
+	} catch (error) {
+		next("Erro ao atualizar o  Usuário" + error);
+	}
+});
 
-router.put('/updateMusics/:userid/:musicId', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        await userServices.updateHeardMusics(parseInt(req.params.userid), parseInt(req.params.musicId));
-        res.json('Usuário atualizado com sucesso!');
-    } catch (error) {
-        next('Erro ao atualizar o  Usuário' + error);
-    }
-})
+router.put("/updateMusics/:userid/:musicId", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await userServices.updateHeardMusics(parseInt(req.params.userid), parseInt(req.params.musicId));
+		res.json("Usuário atualizado com sucesso!");
+	} catch (error) {
+		next("Erro ao atualizar o  Usuário" + error);
+	}
+});
 
-router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const deletedUser = await userServices.delete(parseInt(req.params.id));
-        res.json('Usuário deletado com sucesso!');
-    } catch (error) {
-        next('Erro ao deletar o Usuário' + error);
-    }
-})
+router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const deletedUser = await userServices.delete(parseInt(req.params.id));
+		res.json("Usuário deletado com sucesso!");
+	} catch (error) {
+		next("Erro ao deletar o Usuário" + error);
+	}
+});
 
 export default router;
--- src/domains/Users/services/userServices.ts
+++ src/domains/Users/services/userServices.ts
@@ -1,4 +1,4 @@
-import prisma from '../../../../config/prismaClient'
+import prisma from "../../../../config/prismaClient";
 import { Music, User } from "@prisma/client";
 
 class UserService {
